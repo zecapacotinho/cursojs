7b40013b4dbfb8cf0fdf1965db58ff9757b85dcf
João Carlos Apolinario da Silva <jcasilva1349@gmail.com>
2025-02-14 17:19:47 -0300

    atz

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel =  78.2
+console.log(`A velocidade do seu é de ${vel}km/h`)
+//Condição simples!
+if(vel > 60){ 
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+} 
+console.log(`Dirija sempre com segurança!`)
