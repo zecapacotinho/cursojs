5c3effdf1a40785916cc9118cd2457a28dac9a07
João Carlos Apolinario da Silva <jcasilva1349@gmail.com>
2025-02-16 16:14:14 -0300

    atz

---
+++ aula013/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do{
+    console.log(`passo ${c}`)
+    c++
+} while(c <= 7)
+
+/*
+    var c = 1
+    while(c <= 10){
+        console.log(`passo ${c}`)
+        c++
+    }
+*/ 
+
+
+/*
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+*/
