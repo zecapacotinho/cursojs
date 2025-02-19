539152a8f59368c3e3a1036148249ed309732c8d
João Carlos Apolinario da Silva <jcasilva1349@gmail.com>
2025-02-19 19:18:14 -0300

    atz

--- Aulas/aula15/ambiente.js
+++ Aulas/aula15/ambiente.js
@@ -1,9 +1,17 @@
 let num = [0,8,1,4,7]
 num.push(1)
+let pos = num.indexOf(10)
 num.sort() 
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro valor é do vetor ${num[0]}`)
 for(let pos = 0; pos < num.length; pos++){
     console.log(num[pos])
+}
+
+if(pos == -1){
+    console.log(`Esse valor não foi indentificado`)
+}
+else{
+    console.log(`O valor 8 está na posição ${pos}`)
 }
